4bd0c1439be724106a7a6771bc0b5a2c2ea92766
phillyai <sonagi.philly@gmail.com>
2017-09-04 01:01:01 +0900

    쉘에 인터프리터, 컴파일러 각각 결과 표시 후 시간 측정 추가

--- src/shell.py
+++ src/shell.py
@@ -1,5 +1,26 @@
+from datetime import datetime
+from compiler import compile2bytes
 from interpreter import execute
+from machine import run
+
+def timeelapse(func):
+    def wrapper(*args):
+        before = datetime.now()
+        func(*args)
+        after = datetime.now()
+        print(f'Time elapsed : {(after - before).microseconds}')
+    return wrapper
+
+@timeelapse
+def run_interpreter(code):
+    print(f'Interpreted : {execute(code)}')
+
+@timeelapse
+def run_compiler(code):
+    print(f'Compiled    : {run(compile2bytes(code))}')
 
 while True:
     print('=>', end=' ')
-    print(execute(input()))
+    expr = input()
+    run_interpreter(expr)
+    run_compiler(expr)
