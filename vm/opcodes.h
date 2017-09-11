dc4c80ede9945ec1b0bf780324c55af47393754d
phillyai <sonagi.philly@gmail.com>
2017-09-11 21:20:00 +0900

    C로 구현한 VM 추가

---
+++ vm/opcodes.h
@@ -0,0 +1,21 @@
+#ifndef VM_OPCODES
+#define VM_OPCODES 0
+#define PUSH_CONST_INT 1
+#define PUSH_CONST_REAL 2
+#define RET_VAL 3
+
+#define TERM_PLUS 4
+#define TERM_MINUS 5
+
+#define BIN_ADD 9
+#define BIN_SUB 10
+#define BIN_MUL 11
+#define BIN_DIV 12
+#define BIN_MOD 13
+#define BIN_POW 14
+#define BIN_LSHIFT 15
+#define BIN_RSHIFT 16
+#define BIN_AND 17
+#define BIN_XOR 18
+#define BIN_OR 19
+#endif
--- vm/vm.c
+++ vm/vm.c
@@ -2,21 +2,92 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include "opcodes.h"
 
+#define STACK_SIZE 10
+
+int stack[STACK_SIZE];
+int stindex = 0;
+int program_result = -1;
+
+void push(int i) {
+	stack[stindex++] = i;
+}
+
+int pop() {
+	return stack[--stindex];
+}
+
+int top() {
+	return stack[stindex - 1];
+}
+
+void handle();
+FILE *f;
 int main(int argc, char *argv[]) {
-    // mov eax, 0
-    // add eax, 1
-    // ret
-    unsigned char code[] = {0xb8, 0x00, 0x00, 0x00, 0x00, 0xfe, 0xc3};
-    if (argc < 2) {
-        fprintf(stderr, "Number should be given as argument");
-        return 1;
-    }
+	if (argc < 2) {
+		fprintf(stderr, "File name should be given as argument\n");
+		return 1;
+	}
+	f = fopen(argv[1], "rb");
+	fseek(f, 0, SEEK_END);
+	long len = ftell(f);
+	rewind(f);
+	unsigned char buffer;
+	while (fread(&buffer, (size_t)1, (size_t)1, f) == 1) {
+	  handle(buffer);
+	}
+	fprintf(stdout, "result: %d\n", program_result);
+	return 0;
+}
 
-    int num = atoi(argv[1]);
-    memcpy(&code[1], &num, 4);
-    void *mem = mmap(NULL, sizeof(code), PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_PRIVATE, -1, 0);
-    memcpy(mem, code, sizeof(code));
-    int (*func)() = mem;
-    return func();
+void handle(int code) {
+	fprintf(stdout, "code: %d, top: %d\n", code, top());
+	if (code == PUSH_CONST_INT)	{
+		unsigned char buffer[4];
+		fread(&buffer, (size_t)4, (size_t)1, f);
+		int param = *(int *) buffer;
+		push(param);
+		return;
+	}
+	if (code == PUSH_CONST_REAL) {
+		
+	}
+	if (code >= BIN_ADD) {
+		int left = pop();
+		int right = pop();
+		int res = 0;
+		switch (code) {
+			case BIN_ADD: res = left + right; break;
+			case BIN_SUB: res = left - right; break;
+			case BIN_MUL: res = left * right; break;
+			case BIN_DIV: res = left / right; break;
+		}
+		push(res);
+		return;
+	}
+	if (code == TERM_PLUS) return;
+	if (code == TERM_MINUS) {
+		push(-pop());
+		return;
+	}
+	if (code == RET_VAL) {
+		program_result = pop();
+		return;
+	}
+}
+
+void test_jit(void) {
+	// mov eax, 0
+	// add eax, 1
+	// ret
+	unsigned char code[] = { 0xb8, 0x00, 0x00, 0x00, 0x00, 0x83, 0xc0, 0x01, 0xc3 };
+
+	int num = 42;
+	memcpy(&code[1], &num, 4);
+	void *mem = mmap(NULL, sizeof(code), PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	memcpy(mem, code, sizeof(code));
+	int(*func)() = mem;
+	int res = func();
+	fprintf(stdout, "%d\n", res);
 }
